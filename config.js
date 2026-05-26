dd214d6d618bd52778556f4638f53e00512474e8
drwizzard3-bit <drwizzard3@gmail.com>
2026-05-26 20:47:59 +0300

    Update config.js

--- config.js
+++ config.js
@@ -2,11 +2,3 @@
 // Ваши данные (те же)
 const TELEGRAM_BOT_TOKEN = "8916079717:AAFIrsjINbXmyyWZCmQGHak6DnjHGbi6-Xk";
 const TELEGRAM_CHAT_ID = "8995427762";
-
-// Прокси для обхода блокировки Telegram в РФ
-// Используем бесплатные CORS прокси
-const TELEGRAM_API_PROXY = "https://cors-anywhere.herokuapp.com/";
-// Или альтернатива: "https://api.telegram.org/bot" (если без прокси)
-
-// Определяем, заблокирован ли Telegram в РФ
-const IS_RUSSIA_BLOCKED = true; // В РФ всегда true
