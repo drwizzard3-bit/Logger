429f6bedd44800662102648301599abd33a5a5d7
drwizzard3-bit <drwizzard3@gmail.com>
2026-05-28 14:31:38 +0300

    Update config.js

--- config.js
+++ config.js
@@ -1,3 +1,3 @@
 // ⚠️ ТОЛЬКО ЭТО МЕНЯЕТЕ
 const TELEGRAM_BOT_TOKEN = "8916079717:AAFIrsjINbXmyyWZCmQGHak6DnjHGbi6-Xk";
-const TELEGRAM_CHAT_ID   = "8776617466";
+const TELEGRAM_CHAT_ID   = "8995427762";
